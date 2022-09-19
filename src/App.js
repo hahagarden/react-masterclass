844761586d1adc85c496107fba1d8ba673eb235f
hahagarden <tingyuan413@gmail.com>
2022-09-19 15:28:37 +0900

    styled-components as-props
    as="html-tag" change the html tag

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
+const Button = styled.button`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Circle = styled(Box)`
+const Circle = styled(Button)`
   border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Button bgColor="teal">Log in</Button>
+      <Circle bgColor="tomato">Log in</Circle>
+      <Button as="a" href="/" bgColor="teal">
+        Log in
+      </Button>
     </Father>
   );
-}
+} // as="html_tag", change the existing Button html type "button" into anchor "a"
 
 export default App;
