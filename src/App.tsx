992a4ec5d533e71ee6782d9d727dc6394a2e9047
hahagarden <tingyuan413@gmail.com>
2022-09-24 01:03:38 +0900

    Router, Params
    npm i react-router-dom@5.3.0 react-query
    useParams in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { useState } from "react";
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event; //ES6문법, const value=event.currentTarget.value
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
