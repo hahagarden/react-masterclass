ad593718261b0410f3841d74e2cd3698fb99f1c5
hahagarden <tingyuan413@gmail.com>
2022-09-24 19:52:11 +0900

    fetch data (coinpaprika API)

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
   padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,11 +20,11 @@ const CoinsList = styled.ul``;
 const Coin = styled.li`
   background-color: ${(props) => props.theme.textColor};
   color: ${(props) => props.theme.bgColor};
-  margin: 10px;
+  margin-bottom: 10px;
   a {
     padding: 20px;
     display: block;
-    transition: color 0.2s ease-in;
+    transition: color 0.1s ease-in;
   }
   &:hover {
     a {
@@ -32,39 +35,28 @@ const Coin = styled.li`
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
 `;
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-]; //temporary data array
+interface CoinsInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinsInterface[]>([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+    })(); // (function)() 함수 실행될 때 바로 실행
+  }, []);
   return (
     <Container>
       <Header>
