719b39d0c3c016e19b834fdc6c611478678f3625
hahagarden <tingyuan413@gmail.com>
2022-09-24 23:37:49 +0900

    Route State *
    Coins.tsx에서 API를 호출하고 그 정보를 Coin.tsx로 route state로 보내주므로 꼭 Coins를 통해서 Coin url로 접속해야 state가 전달됨.
    직접 url에 /coinId쳐서 들어간경우 state 존재하지 않아 Loading 현시

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,50 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface Params {
   coinId: string;
 }
 
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.div`
+  text-align: center;
+  font-size: 20px;
+`;
+
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<Params>();
-  return <h1>Coin: {coinId}</h1>;
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>Coin {state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  ); //state occurs in Home(Coins.tsx), so if type url "/coinId" directly, state does not exist.
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -81,7 +81,9 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>
+              <Link
+                to={{ pathname: `/${coin.id}`, state: { name: coin.name } }}
+              >
                 <img
                   src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
                 />
