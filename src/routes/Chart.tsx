3f128ed17b0092609450c6093d0e3658f49be61e
hahagarden <tingyuan413@gmail.com>
2022-09-26 17:15:27 +0900

    fetch coinhistory in Chart.tsx

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,27 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "./api";
+
+interface ChartProps {
+  coinId: string;
+}
+
+interface IData {
+  close: string;
+  high: string;
+  low: string;
+  market_cap: number;
+  open: string;
+  time_close: number;
+  time_open: number;
+  volume: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IData[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
+  return <div></div>;
 }
 
 export default Chart;
