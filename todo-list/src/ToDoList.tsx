e5c72172e665440767a62b9176df5a76f396594f
hahagarden <tingyuan413@gmail.com>
2022-09-27 22:56:24 +0900

    react-hook-form formState errors
    useForm<IForm>({defaultvalues:{email:"@naver.com"})
    
    <span>errors?.email?.message</span>
    
    pattern: using regular expression

--- todo-list/src/ToDoList.tsx
+++ todo-list/src/ToDoList.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  username?: string;
+  password: string;
+  passwordcheck?: string;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
-  const onValid = (data: any) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
     console.log(data);
   };
-  console.log(formState.errors);
+  console.log(errors);
   return (
     <div>
       <form
@@ -14,10 +29,18 @@ function ToDoList() {
         onSubmit={handleSubmit(onValid)}
       >
         <input
-          {...register("email", { required: true, minLength: 3 })}
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
           placeholder="email"
         />
+        <span>{errors?.email?.message}</span>
         <input {...register("username")} placeholder="username" />
+        <span>{errors?.username?.message}</span>
         <input
           {...register("password", {
             required: "password is required",
@@ -25,7 +48,9 @@ function ToDoList() {
           })}
           placeholder="password"
         />
+        <span>{errors?.password?.message}</span>
         <input {...register("passwordcheck")} placeholder="password check" />
+        <span>{errors?.passwordcheck?.message}</span>
         <button>submit</button>
       </form>
     </div>
