66505eb5e5de3aee15c372a48f043b4401cc5934
hahagarden <tingyuan413@gmail.com>
2022-10-02 21:22:14 +0900

    Style global, dnd styles

--- trello-clone/src/index.tsx
+++ trello-clone/src/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { ThemeProvider } from "styled-components";
+import { RecoilRoot } from "recoil";
 import App from "./App";
+import GlobalStyle from "./styles/GlobalStyle";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+);
---
+++ trello-clone/src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#38b0e7",
+  boardColor: "#e3e3e3",
+  cardColor: "white",
+};
