6f2d59c36ed72088423e54e9d4e2d80f1e0904a7
hahagarden <tingyuan413@gmail.com>
2022-10-14 17:57:07 +0900

    slider using AnimatePresence

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -6,6 +6,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -13,34 +14,50 @@ const Wrapper = styled(motion.div)`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  position: absolute;
+  top: 100px;
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: white;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+  font-size: 20px;
 `;
 
 const boxVariants = {
-  initial: { opacity: 0, scale: 0 },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  leaving: {
+  invisible: { x: 500, opacity: 0, scale: 0 },
+  visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+  exit: {
+    x: -500,
     opacity: 0,
-    y: 20,
+    scale: 0,
+    transition: { duration: 1 },
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={boxVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
