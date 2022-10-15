856aee2adc79728c8324f17c04a2f123a814671f
hahagarden <tingyuan413@gmail.com>
2022-10-16 05:37:24 +0900

    routes, globalStyle, theme

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ netflix-clone/src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  red: "#E51013",
+  black: {
+    darker: "#141414",
+    dark: "#181818",
+    light: "#2F2F2F",
+  },
+  white: {
+    light: "#fff",
+    dark: "#e5e5e5",
+  },
+};
