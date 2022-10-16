854dd5634e490334c4317f01d25f340731a1c4f0
hahagarden <tingyuan413@gmail.com>
2022-10-16 17:40:18 +0900

    feat&design: header

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
@@ -7,17 +8,12 @@ import Tv from "./Routes/Tv";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/tv">
-          <Tv />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-      </Switch>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
