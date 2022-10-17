baf14771f71ce4b25f493916f716e2cff5bdc209
hahagarden <tingyuan413@gmail.com>
2022-10-17 14:35:41 +0900

    feat(API): get nowPlaying data, make banner

--- netflix-clone/src/Routes/Home.tsx
+++ netflix-clone/src/Routes/Home.tsx
@@ -1,6 +1,59 @@
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { useQuery } from "react-query";
+import { makeImagePath } from "./utils";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgImage: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgImage});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 24px;
+  width: 50%;
+`;
+
 function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  ); // useQuery: fetcher로 data와 로딩중여부를 알려줌
+  console.log(data, isLoading);
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>Home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgImage={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
---
+++ netflix-clone/src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  totla_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+} //fetcher function: 데이터를 받아오고 JSON을 리턴하는 함수
